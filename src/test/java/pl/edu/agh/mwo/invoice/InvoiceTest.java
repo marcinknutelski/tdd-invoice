b0260cdea70edb1cbee4d21768347b9e7c98112d
Student21 <Student21@S7N-PROGLANG-12.pcoip.ki.agh.edu.pl>
2019-02-10 10:37:07 +0100

    Add Test for different number invoice

--- src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
+++ src/test/java/pl/edu/agh/mwo/invoice/InvoiceTest.java
@@ -104,4 +104,42 @@ public class InvoiceTest {
 	public void testInvoiceWithNegativeQuantity() {
 		invoice.addProduct(new DairyProduct("Zsiadle mleko", new BigDecimal("5.55")), -1);
 	}
+	
+	@Test
+	public void testInvoiceHasNumber() {
+		// invoice.getNumber();
+		Integer number = invoice.getNumber();
+		Assert.assertNotNull(number);
+	}
+	
+	@Test
+	public void testInvoiceNumberIsGreaterThanZero() {
+		Integer number = invoice.getNumber();
+		Assert.assertThat(number, Matchers.greaterThan(0));
+	}
+	
+	@Test
+	public void testTwoInvoicesHasDifferentNumber() {
+		Integer number1 = invoice.getNumber();
+		Integer number2 = new Invoice().getNumber();
+		Assert.assertNotEquals(number1, number2);
+	}
+	
+	@Test
+	public void testTheSameInvoiceHasTheSameNumber() {
+		Integer number1 = invoice.getNumber();
+		Integer number2 = invoice.getNumber();
+		Assert.assertEquals(number1, number2);
+	}
+	
+	@Test
+	public void testTheSecondInvoiceHasGreaterNumber() {
+		for (int i = 0; i < 100; i++) {
+			Integer number1 = invoice.getNumber();
+			Integer number2 = new Invoice().getNumber();
+			Assert.assertThat(number1, Matchers.lessThan(number2));
+		}
+	}
+	
+		
 }
